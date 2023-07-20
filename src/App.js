6c5202f69ea5196afa850bd9c92ebe4479bb66ad
heladiomhtech <130695499+heladiosampaiotech@users.noreply.github.com>
2023-07-20 11:08:36 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Contatos from './components/pages/contatos/index';
 import Fotos from './components/pages/fotos/index';
 import Comentarios from './components/pages/comentarios/index';
+import Logo from '../src/assets/logo.png'
 
 function App() {
  
@@ -12,6 +13,7 @@ function App() {
     
      <BrowserRouter>
       <Routes>
+        <Route path='/marioBros' elemento={Logo}/>
         <Route path='/marioBros' element={<Home/>}/>
         <Route path='/Contatos' element={<Contatos/>}/>
         <Route path='/Fotos' element={<Fotos/>}/>
