fb96885b998decda31b71575acbb00c2e8b90562
heladiomhtech <130695499+heladiosampaiotech@users.noreply.github.com>
2023-07-20 10:29:04 -0300

    Update index.js

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -7,7 +7,7 @@ function Header (){
 
     return(
         <header>
-            <Link to={'/'}>
+            <Link to={'/marioBros'}>
                 <img id="logo" src={Logo}/>
             </Link>
             <nav>
